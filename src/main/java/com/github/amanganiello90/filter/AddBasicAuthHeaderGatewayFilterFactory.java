525c40a84ca500d78623a1f1d44650888d06edaa
Angelo Manganiello <angelo.mang@libero.it>
2019-01-02 14:35:21 +0100

    Update AddBasicAuthHeaderGatewayFilterFactory.java

--- src/main/java/com/github/amanganiello90/filter/AddBasicAuthHeaderGatewayFilterFactory.java
+++ src/main/java/com/github/amanganiello90/filter/AddBasicAuthHeaderGatewayFilterFactory.java
@@ -2,21 +2,28 @@ package com.github.amanganiello90.filter;
 
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 
 @Component
-public class AddBasicAuthHeaderGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
+public class AddBasicAuthHeaderGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
   
   private static final String HEADER_KEY = "Authorization";
   private static final String BASIC_PREFIX = "Basic ";
   
+  @Value("${gateway.basic.auth.username:''}")
+  private String basicUsername;
+  
+  @Value("${gateway.basic.auth.password:''}")
+  private String basicPassword;
+  
   @Override
-  public GatewayFilter apply(NameValueConfig config) {
+  public GatewayFilter apply(Object config) {
     StringBuilder sbHeaderValue = new StringBuilder(BASIC_PREFIX);
-    StringBuilder authCode = new StringBuilder(config.getName()).append(":").append(config.getValue());
+    StringBuilder authCode = new StringBuilder(basicUsername).append(":").append(basicPassword);
     String authorizationH =
         sbHeaderValue.append(Base64.getEncoder().encodeToString(authCode.toString().getBytes())).toString();
     return (exchange, chain) -> {
@@ -29,3 +36,4 @@ public class AddBasicAuthHeaderGatewayFilterFactory extends AbstractNameValueGat
   }
   
 }
+
